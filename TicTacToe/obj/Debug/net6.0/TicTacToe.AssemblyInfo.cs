00b52f771f3e6427a3324b7aad1fbbd4799496c3
Jonathan-Pruitt <130386433+Jonathan-Pruitt@users.noreply.github.com>
2024-04-29 15:21:46 -0500

    TicTacToe Updates

--- TicTacToe/obj/Debug/net6.0/TicTacToe.AssemblyInfo.cs
+++ TicTacToe/obj/Debug/net6.0/TicTacToe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d824659ac62055c50bac6dac152682699fb3b36")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
